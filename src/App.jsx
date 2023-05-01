009f99f0df80eaf17702c8622b8aa5efca4c933b
giantyo <giantyoard@gmail.com>
2023-05-02 01:09:23 +0700

    Fix PrivateRoute

--- src/App.jsx
+++ src/App.jsx
@@ -82,22 +82,22 @@ function App() {
               <Route element={<PrivateRouteUser />}>
                 <Route path="/series" element={<Series />} />
                 <Route path="/movies" element={<Movies />} />
+                <Route path="/user-profile" element={<UserProfile />} />
+                <Route path="/user-payment" element={<UserPayment />} />
                 <Route path="/films-detail/:id" element={<FilmsDetail />} />
-
-                <Route element={<PrivateRouteAdmin />}>
-                  <Route path="/user-profile" element={<UserProfile />} />
-                  <Route path="/user-payment" element={<UserPayment />} />
-                  <Route path="/admin-list-film" element={<AdminListFilm />} />
-                  <Route path="/admin-transaction" element={<Transaction />} />
-                  <Route path="/admin-add-film" element={<AdminAddFilm />} />
-                  <Route path="/films-detail/:id" element={<FilmsDetail />} />
-
-                  <Route
-                    exact
-                    path="/admin-edit-film/:id"
-                    element={<AdminEditFilm />}
-                  />
-                </Route>
+              </Route>
+              <Route element={<PrivateRouteAdmin />}>
+                <Route path="/series" element={<Series />} />
+                <Route path="/movies" element={<Movies />} />
+                <Route path="/admin-list-film" element={<AdminListFilm />} />
+                <Route path="/admin-transaction" element={<Transaction />} />
+                <Route path="/admin-add-film" element={<AdminAddFilm />} />
+                <Route path="/films-detail/:id" element={<FilmsDetail />} />
+                <Route
+                  exact
+                  path="/admin-edit-film/:id"
+                  element={<AdminEditFilm />}
+                />
               </Route>
             </Route>
           </Routes>
