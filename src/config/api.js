423c0c7f3bca73bf1e5015125e751c2884f3d101
giantyo <giantyoard@gmail.com>
2023-05-01 23:48:03 +0700

    FE env fix

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create base URL API
 export const API = axios.create({
-  baseURL:'import.meta.env.VITE_REACT_APP_BASE_URL',
+  baseURL:'import.meta.env.VITE_BASE_API_URL',
 });
 
 // Set Authorization Token Header
