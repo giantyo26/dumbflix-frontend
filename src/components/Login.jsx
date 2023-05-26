8db0e60908212d2087eb5b486919a7154eb056db
giantyo26 <72968195+giantyo26@users.noreply.github.com>
2023-05-27 06:52:26 +0700

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,14 +4,10 @@ import { UserContext } from "../context/UserContext";
 import { useMutation } from "react-query";
 import { useNavigate } from "react-router-dom";
 import Swal from 'sweetalert2'
-import Register from "./Register";
-
-
 
 export default function Login() {
     const [showLogin, setShowLogin] = useState(false);
-    const [showRegister, setShowRegister] = useState(false);
-    const [_, dispatch] = useContext(UserContext);
+    const [_, dispatch] = useContext(UserContext)
 
     const navigate = useNavigate();
     const handleLoginModal = (event) => {
@@ -19,11 +15,7 @@ export default function Login() {
             setShowLogin(false);
         }
     };
-     // To show register modal
-    const handleRegisterClick = (event) => {
-        setShowLogin(false);
-        setShowRegister(true);
-    }
+
     // Login logic
     const [loginValue, setLoginValue] = useState({
         email: "",
@@ -125,13 +117,16 @@ export default function Login() {
                                 Login
                             </button>
                             <p className="text-white">
-                                Don't have an account ?<span onClick={handleRegisterClick}>Click<a className="font-semibold">{" "}Here</a></span>
+                                Don't have an account ? <span>Click</span>
+                                <a className="font-semibold" href="#">
+                                    {" "}
+                                    Here
+                                </a>
                             </p>
                         </form>
                     </div>
                 </div>
             )}
-            {showRegister && <Register />}
         </>
     );
 }
